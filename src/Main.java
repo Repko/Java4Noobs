790bff97c8d4b6b2bf8608ef54a435283990a7ef
unknown <milanrepic@gmail.com>
2019-02-02 15:32:00 +0000

    this is very first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[]args){
+        int number;
+        String name = "milan"   ;
+                System.out.println("Milan is better than Daniel!");
+
+                //this is the initial git commit
+        
+    }
+}
